e2efec6c9e4ac08adec0c95dd93d59a9f7148788
ensea <ensea@StudentLab>
2024-12-16 09:51:11 +0100

    End of quesiton 5

--- P6.c
+++ P6.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <sys/wait.h>
+#include <time.h>
 
 #define SIZE 4096
 
@@ -12,14 +13,20 @@ void printPrompt(int status,long duration) {
 
     if (WIFEXITED(status)) { 
         int exit_status = WEXITSTATUS(status);
-        snprintf(prompt, SIZE, "enseash [exit:%d] %% ", exit_status,duration);
+        snprintf(prompt, SIZE, "enseash [exit:%d|%ldms] %% ", exit_status, duration);
     } else if (WIFSIGNALED(status)){
         int signal = WTERMSIG(status);
-        snprintf(prompt, SIZE, "enseash [sign:%d] %% ", signal,duration);
+        snprintf(prompt, SIZE, "enseash [sign:%d|%ldms] %% ", signal, duration);
     } else { 
         snprintf(prompt, SIZE, "enseash %% ");
     }
     write(STDOUT_FILENO, prompt, strlen(prompt));
+}
+
+long Duration(struct timespec start, struct timespec end) {
+    long seconds = end.tv_sec - start.tv_sec;
+    long nanoseconds = end.tv_nsec - start.tv_nsec;
+    return seconds * 1000 + nanoseconds / 1000000;
 }
 
 int main() {
@@ -27,13 +34,14 @@ int main() {
     ssize_t bytes_read;
     const char *prompt;
     int last_status = 0; // initialized for Q4
+    long duration = 0; // Initialized for Q5
 
     const char *message = "$ ./enseash \nBienvenue dans la shell ENSEA. \nPour quitter, tapez 'exit'\n";
     write(STDOUT_FILENO, message, strlen(message));
 
     const char *message_exit = "Bye bye...\n";
     while(1){
-        printPrompt(last_status);
+        printPrompt(last_status,duration);
 
 
         bytes_read = read(STDIN_FILENO, buf, SIZE-1);
@@ -61,6 +69,11 @@ if (strcmp(buf,"exit")==0){
         { // Deals with the case when pressing "Enter"
             continue;
         }
+
+
+        struct timespec start_time, end_time;
+        clock_gettime(CLOCK_MONOTONIC, &start_time);
+
         pid_t process = fork();
 
         if (process < 0)
@@ -78,6 +91,8 @@ if (strcmp(buf,"exit")==0){
         } else { // Parent process
             
             waitpid(process, &last_status, 0);
+            clock_gettime(CLOCK_MONOTONIC, &end_time);
+            duration = Duration(start_time, end_time); // Calcul de la durée
         }
     
         
