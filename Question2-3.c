b39880477346b783f4cc501ae71ea9e7c79ccc9d
ensea <ensea@StudentLab>
2024-12-16 11:13:19 +0100

    End of question 2 and 3

--- Question2-3.c
+++ Question2-3.c
@@ -13,30 +13,30 @@ int main() {
     char buf[SIZE];
     ssize_t bytes_read;
     const char *prompt;
-
-
     const char *message = "$ ./enseash \nBienvenue dans la shell ENSEA. \nPour quitter, tapez 'exit'\n";
-    write(STDOUT_FILENO, message, strlen(message));
-
     const char *message_exit = "Bye bye...\n";
+
+
+    write(STDOUT_FILENO, message, strlen(message)); // Write the introduction to the shell
+
+
     while(1){
         write(STDOUT_FILENO, "enseash % ", 10);
 
-        bytes_read = read(STDIN_FILENO, buf, SIZE-1);
-
+        bytes_read = read(STDIN_FILENO, buf, SIZE-1);  // Reads the input command of the user
         buf[bytes_read]='\0';
         buf[strcspn(buf,"\n")]='\0';
 
-if (strcmp(buf,"exit")==0){
-    write(STDOUT_FILENO,message_exit,strlen(message_exit));
-    exit(EXIT_SUCCESS);
-}
+    if (strcmp(buf,"exit")==0){ // If the written command corresponds to "exit", writes the exit message
+        write(STDOUT_FILENO,message_exit,strlen(message_exit));
+        exit(EXIT_SUCCESS);
+    }
 
- int argc = 0;
+    int argc = 0;
     char *argv[SIZE] = {};
     char *token = strtok(buf, " ");
 
-    // Configuration of argv
+    // Configuration of argv and splitting it in two parts (command and arguments) [Question 2 and 6 at the same time]
     while (token != NULL && argc < ((SIZE)-1))
     {
         argv[argc++] = token;
@@ -46,29 +46,31 @@ if (strcmp(buf,"exit")==0){
 
     if (argc == 0)
     { // Deals with the case when pressing "Enter"
-        return -1;
+        continue;
     }
+
+
+
     pid_t process = fork();
 
     if (process < 0)
-    {
+    { // Handles possible errors when calling the fork
         perror("Fork failed");
         return -1;
     }
 
-   if (process == 0) { // Child Process
-            execvp(argv[0], argv);
+    if (process == 0) { // Child Process
+        execvp(argv[0], argv);
 
 
-            perror("Error: Command not found");
-            exit(EXIT_FAILURE);
-        } else { // Parent process
-            int status;
-            waitpid(process, &status, 0);
+        perror("Error: Command not found");
+        exit(EXIT_FAILURE);
+    } else { // Parent process
+        int status;
+        waitpid(process, &status, 0);
         }
     
         
-
     }
     exit(EXIT_SUCCESS);
 }
